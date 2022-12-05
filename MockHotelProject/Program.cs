1a2297af207309900a6819ea43713ec97eff531d
avinmano <avi_mano@hotmail.com>
2022-12-05 23:14:53 +0100

    Modified appsettings

--- MockHotelProject/Program.cs
+++ MockHotelProject/Program.cs
@@ -27,19 +27,11 @@ app.UseHttpsRedirection();
 
 
 
-//app.MapGet("/weatherforecast", () =>
-//{
-//    var forecast = Enumerable.Range(1, 5).Select(index =>
-//        new WeatherForecast
-//        (
-//            DateTime.Now.AddDays(index),
-//            Random.Shared.Next(-20, 55),
-//            summaries[Random.Shared.Next(summaries.Length)]
-//        ))
-//        .ToArray();
-//    return forecast;
-//})
-//.WithName("GetWeatherForecast");
+app.MapGet("/weatherforecast", () =>
+{
+   return "Hello World";
+})
+.WithName("GetWeatherForecast");
 
 app.Run();
 
